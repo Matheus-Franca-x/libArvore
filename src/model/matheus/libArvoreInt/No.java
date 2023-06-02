361a3463420e8e1f61289b3f79656b58aba3a4e8
Matheus França <matheussss6613@gmail.com>
2023-06-02 01:26:49 -0300

    att eclipse 17.0

---
+++ src/model/matheus/libArvoreInt/No.java
@@ -0,0 +1,14 @@
+package model.matheus.libArvoreInt;
+
+public class No {
+
+	int dado;
+	No esquerda;
+	No direita;
+
+	@Override
+	public String toString() {
+		return "No [dado=" + dado + "]";
+	}
+
+}
